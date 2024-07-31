8f8e5b6fe44552bd8d055ac1075dbe435b5eef93
Kousalya422 <kousy.felino@gmail.com>
2024-07-31 11:49:13 +0530

    Update urls.py

--- demo/urls.py
+++ demo/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', views.user_login, name='login'),
+    path('', views.user_login, name='login'),
     path('home/', views.home, name='home'),
     path('generate_password/', views.generate_password, name='generate_password'),
     path('logout/', views.user_logout, name='logout'),
